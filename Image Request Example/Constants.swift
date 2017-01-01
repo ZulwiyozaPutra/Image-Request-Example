3914c0dd57056d3e49a57e82f9fe8922c9eb4b4c
Zulwiyoza Putra <zulwiyozaputra@yahoo.com>
2017-01-01 20:03:24 +0700

    refactored

---
+++ Image Request Example/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Image Request Example
+//
+//  Created by Zulwiyoza Putra on 1/1/17.
+//  Copyright © 2017 Zulwiyoza Putra. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let CatURL = "https://upload.wikimedia.org/wikipedia/commons/4/4d/Cat_November_2010-1a.jpg"
+    static let PuppyURL = "http://www.puppiesden.com/pics/1/poodle-puppy2.jpg"
+}
--- Image Request Example/ViewController.swift
+++ Image Request Example/ViewController.swift
@@ -9,28 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Outlets
     @IBOutlet weak var imageView: UIImageView!
     
+    //Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //Setup the URL of image
-        let imageURL = URL(string: "https://upload.wikimedia.org/wikipedia/commons/4/4d/Cat_November_2010-1a.jpg")!
+        let imageURL = URL(string: Constants.PuppyURL)!
         
+        //Create a network request
         let task = URLSession.shared.dataTask(with: imageURL) { (data, response, error) in
             
             if error == nil {
+                
+                //Create an image
                 let downloadedImage = UIImage(data: data!)
                 
-                DispatchQueue.main.async {
+                //Update the UI
+                performUIUpdatesOnMain {
                     self.imageView.image = downloadedImage
                 }
+
+            } else {
+                print(error!)
             }
             
             print("task finished")
         }
         
+        //Start network request
         task.resume()
     }
 
