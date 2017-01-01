b2b217da1e158f77f8d97accb59e0dfde909c3fc
Zulwiyoza Putra <zulwiyozaputra@yahoo.com>
2017-01-01 18:58:48 +0700

    HTTP request now is allowed

--- Image Request Example/ViewController.swift
+++ Image Request Example/ViewController.swift
@@ -10,12 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //Setup the URL of image
-        let imageURL = URL(string: "https://upload.wikimedia.org/wikipedia/commons/4/4d/Cat_November_2010-1a.jpg")!
+        let imageURL = URL(string: "http://www.mydogtrainingonline.com/images/how-to-train-your-dog.jpg")!
         
+        let task = URLSession.shared.dataTask(with: imageURL) { (data, response, error) in
+            
+            if error == nil {
+                let downloadedImage = UIImage(data: data!)
+                
+                DispatchQueue.main.async {
+                    self.imageView.image = downloadedImage
+                }
+            }
+            
+            print("task finished")
+        }
+        
+        task.resume()
     }
 
 }
